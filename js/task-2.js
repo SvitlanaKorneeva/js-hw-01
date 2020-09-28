b69e9dad31820250da71ac3a63c6f39d567921de
SvitlanaKorneeva <68194276+SvitlanaKorneeva@users.noreply.github.com>
2020-09-28 23:39:36 +0300

    js

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+const invoice = 150;
+const stock = 100;
+
+// Write code under this line
+const message =
+  invoice <= stock
+    ? "Заказ оформлен, с вами свяжется менеджер"
+    : "На складе недостаточно товаров!";
+
+// Write code under this line
+console.log(message);
